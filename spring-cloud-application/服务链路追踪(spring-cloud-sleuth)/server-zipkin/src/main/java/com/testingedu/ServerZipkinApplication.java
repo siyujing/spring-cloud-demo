77bb920f203e88c5c4e666853b0e9963740131f1
syj <siyujing@boxfish.cn>
2018-09-30 18:26:12 +0800

    [B]

--- spring-cloud-application/服务链路追踪(spring-cloud-sleuth)/server-zipkin/src/main/java/com/testingedu/ServerZipkinApplication.java
+++ spring-cloud-application/服务链路追踪(spring-cloud-sleuth)/server-zipkin/src/main/java/com/testingedu/ServerZipkinApplication.java
@@ -4,6 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import zipkin.server.EnableZipkinServer;
 
+/**
+ 在spring Cloud为F版本的时候，已经不需要自己构建Zipkin Server了，只需要下载jar即可，
+ 方法一 安装zipkin linux安装路径：
+ wget -O zipkin.jar 'https://search.maven.org/remote_content?g=io.zipkin.java&a=zipkin-server&v=LATEST&c=exec'
+ nohup java -jar zipkin.jar &
+ 访问浏览器localhost:9411（默认端口号9411）
+
+ 方法二 下载地址：
+ https://dl.bintray.com/openzipkin/maven/io/zipkin/java/zipkin-server/
+ 方法二下载完成后运行jar包：java -jar zipkin-server-2.10.1-exec.jar
+ 访问浏览器localhost:9494
+
+ * */
 @SpringBootApplication
 @EnableZipkinServer
 public class ServerZipkinApplication {
